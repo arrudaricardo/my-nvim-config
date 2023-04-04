1ccb24328510e49d5b4d2f53fb9c701b8876467f
Ricardo de Arruda <settiricardo@gmail.com>
2023-04-04 16:47:10 +0100

    remove wk

--- after/plugin/overseer.lua
+++ after/plugin/overseer.lua
@@ -1,15 +1,4 @@
-local wk = require("which-key")
-
 require('overseer').setup {}
-
-wk.register({
-    ["<leader>o"] = {
-        name = "Overseer",
-        o = { "<cmd>:OverseerToggle<cr>", "Toggle" },
-        r = { "<cmd>:OverseerRun<cr>", "Run" },
-        b = { "<cmd>:OverseerBuild<cr>", "Build" },
-    },
-})
 
 vim.keymap.set('n', '<leader>oo', "<cmd>:OverseerToggle<cr>",{ noremap = true, silent = true, desc = 'Overseer Toggle'})
 vim.keymap.set('n', '<leader>or', "<cmd>:OverseerRun<cr>",{ noremap = true, silent = true, desc = 'Overseer Run'})
