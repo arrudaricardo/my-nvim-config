d49a12f947b6f937cdfbe06c7cf6f4eba4b5ee36
Ricardo de Arruda <settiricardo@gmail.com>
2023-04-04 16:53:22 +0100

    add desc keymap

--- lua/arrudaricardo/remap.lua
+++ lua/arrudaricardo/remap.lua
@@ -9,24 +9,21 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "Q", "<nop>", { desc = "Nop" })
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>", { desc = "Make it Rain" });
+vim.keymap.set("n", "<leader>mg", "<cmd>CellularAutomaton game_of_life<CR>", { desc = "Meke it Game of Life" });
 
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+vim.keymap.set("n", "H", "<cmd>BufferLineCyclePrev<CR>", { desc = "Cycle Previous Buffer" })
+vim.keymap.set("n", "L", "<cmd>BufferLineCycleNext<CR>", { desc = "Cycle Next Buffer" })
 
-vim.keymap.set("n", "H", "<cmd>BufferLineCyclePrev<CR>")
-vim.keymap.set("n", "L", "<cmd>BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save File" })
+vim.keymap.set("n", "<leader>W", "<cmd>wa<cr>", { desc = "Save All Files" })
+vim.keymap.set("n", "<leader>c", "<cmd>bw<cr>", { desc = "Close Buffer" })
 
-vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
-vim.keymap.set("n", "<leader>c", "<cmd>bw<cr>")
+vim.keymap.set("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", { desc = "Format" })
 
-vim.keymap.set("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
-
-vim.keymap.set("n", "<C-H>", "<C-W>h")
-vim.keymap.set("n", "<C-J>", "<C-W>j")
-vim.keymap.set("n", "<C-K>", "<C-W>k")
-vim.keymap.set("n", "<C-L>", "<C-W>l")
+vim.keymap.set("n", "<C-H>", "<C-W>h", { desc = "Move to window left" })
+vim.keymap.set("n", "<C-J>", "<C-W>j", { desc = "Move to window below" })
+vim.keymap.set("n", "<C-K>", "<C-W>k", { desc = "Move to window above" })
+vim.keymap.set("n", "<C-L>", "<C-W>l", { desc = "Move to window right" })
