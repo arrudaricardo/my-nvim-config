07ed2f676064cd9e16de38b400f8370a7f5f70b2
Ricardo de Arruda <settiricardo@gmail.com>
2023-04-04 16:30:13 +0100

    wk p3

--- after/plugin/overseer.lua
+++ after/plugin/overseer.lua
@@ -10,3 +10,7 @@ wk.register({
         b = { "<cmd>:OverseerBuild<cr>", "Build" },
     },
 })
+
+vim.keymap.set('n', '<leader>oo', "<cmd>:OverseerToggle<cr>",{ noremap = true, silent = true, desc = 'Overseer Toggle'})
+vim.keymap.set('n', '<leader>or', "<cmd>:OverseerRun<cr>",{ noremap = true, silent = true, desc = 'Overseer Run'})
+vim.keymap.set('n', '<leader>ob', "<cmd>:OverseerBuild<cr>",{ noremap = true, silent = true, desc = 'Overseer Build'})
