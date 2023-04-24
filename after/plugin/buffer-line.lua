cbddb2b5f2cec78d04728e9612f089f9ce622035
Ricardo de Arruda <settiricardo@gmail.com>
2023-04-24 11:14:21 +0100

    fix bufferline

--- after/plugin/buffer-line.lua
+++ after/plugin/buffer-line.lua
@@ -2,10 +2,10 @@ local bufferline = require("bufferline")
 
 bufferline.setup {}
 
-vim.keymap.set('n', '<leader>bb', "<cmd>BufferLineTogglePin<cr>", { noremap = true, silent = true, desc = 'Pin Buffer'})
+vim.keymap.set('n', '<leader>bb', "<CMD>BufferLineTogglePin<CR>", { noremap = true, silent = true, desc = 'Pin Buffer'})
 vim.keymap.set('n', '<leader>bc', "bw", { noremap = true, silent = true, desc = 'Close Buffer'})
-vim.keymap.set('n', '<leader>bf', "<cmd>BufferLinePick<cr>", { noremap = true, silent = true, desc = 'Pick Buffer'})
-vim.keymap.set('n', '<leader>bl', "<cmd>BufferLineMoveNext<cr>", { noremap = true, silent = true, desc = 'Move Next Buffer'})
-vim.keymap.set('n', '<leader>bh', "<cmd>BufferLineMovePrev<cr>", { noremap = true, silent = true, desc = 'Move Prev Buffer'})
-vim.keymap.set('n', '<leader>bL', "<cmd>BufferLineCloseRightcr>", { noremap = true, silent = true, desc = 'Close Right Buffer'})
-vim.keymap.set('n', '<leader>bH', "<cmd>BufferLineCloseLeft>", { noremap = true, silent = true, desc = 'Close Left Buffer'})
+vim.keymap.set('n', '<leader>bf', "<CMD>BufferLinePick<CR>", { noremap = true, silent = true, desc = 'Pick Buffer'})
+vim.keymap.set('n', '<leader>bl', "<CMD>BufferLineMoveNext<CR>", { noremap = true, silent = true, desc = 'Move Next Buffer'})
+vim.keymap.set('n', '<leader>bh', "<CMD>BufferLineMovePrev<CR>", { noremap = true, silent = true, desc = 'Move Prev Buffer'})
+vim.keymap.set('n', '<leader>bL', "<CMD>BufferLineCloseRight<CR>", { noremap = true, silent = true, desc = 'Close Right Buffer'})
+vim.keymap.set('n', '<leader>bH', "<CMD>BufferLineCloseLeft<CR>", { noremap = true, silent = true, desc = 'Close Left Buffer'})
